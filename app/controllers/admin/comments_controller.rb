31cc704da8a8b37f71f46c2361c58e6df6583fec
Seyoung <ksy4961@gmail.com>
2017-05-17 09:14:34 +0900

    First commit

---
+++ app/controllers/admin/comments_controller.rb
@@ -0,0 +1,4 @@
+class Admin::CommentsController < ApplicationController
+  def destory
+  end
+end
---
+++ app/controllers/admin/posts_controller.rb
@@ -0,0 +1,22 @@
+class Admin::PostsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destory
+  end
+
+  def index
+  end
+
+  def show
+  end
+end
